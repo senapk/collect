c66eb93099259f380d1dea6c6f47471a9c6caba6
David Sena <sena.ufc@gmail.com>
2025-02-26 10:31:25 -0300

    Update on qua 26 fev 2025 10:31:25 -03

--- grade.py
+++ grade.py
@@ -39,8 +39,8 @@ def get_user_task_grade(grade: str) -> float:
         return 0
     progress = grade[0:3] #numbers
     progress_value = int(progress) / 2 # 100 // 2 -> 50
-    skill = get_self_grade_value(grade[3]) * 5 # 0 a 25
-    autonomy = get_self_grade_value(grade[4]) * 5 # 0 a 25
+    skill = get_self_grade_value(grade[3]) * 6 # 0 a 30
+    autonomy = get_self_grade_value(grade[4]) * 6 # 0 a 30
     total = (progress_value + skill + autonomy) / 10
     return total
 
