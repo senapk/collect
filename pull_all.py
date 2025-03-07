ed36c7fefcab3dcdc76a47483e7ca02b50164f92
David Sena <sena.ufc@gmail.com>
2025-03-07 09:35:09 -0300

    Update on sex 07 mar 2025 09:35:09 -03

--- pull_all.py
+++ pull_all.py
@@ -26,7 +26,7 @@ for folder in folders:
     os.chdir(folder)
     restore = subprocess.Popen(["git", "restore", "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     clean = subprocess.Popen(["git", "clean", "-fd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    pull = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pull = subprocess.Popen(["git", "pull", "origin", "main"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     processes.append((folder, restore, clean, pull))
     os.chdir(root)
 
