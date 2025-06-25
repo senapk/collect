c63c02834a5463837477c4ada5f71a3abc86fc1c
David Sena Oliveira <sena.ufc@gmail.com>
2025-06-25 19:20:47 -0300

    fup graph

--- src/turmas/collect.py
+++ src/turmas/collect.py
@@ -4,6 +4,22 @@ import argparse
 import subprocess
 import json
 
+class JsonTask:
+    def __init__(self):
+        self.path = ""
+        self.folder = ""
+        self.rep = ""
+
+    def __str__(self):
+        return f"{self.folder} {self.rep}"
+
+    def __repr__(self):
+        return f"JsonTask({self.folder}, {self.rep})"
+    
+    def load_from_json(self, json_data: dict[str, str]):
+        self.folder = json_data.get("folder", self.folder)
+        self.rep = json_data.get("rep", self.rep)
+
 class Target:
     def __init__(self, folder: str, rep: str):
         self.folder = folder
@@ -15,21 +31,22 @@ class Target:
     def __repr__(self):
         return f"Target({self.folder}, {self.rep})"
 
-def get_user_graph(target: Target):
+def get_user_graph(target: Target) -> str:
     folder = target.folder
     rep = target.rep
     print(".." + folder)
-    folder_name = os.path.basename(folder)
-    basedir = os.path.dirname(folder)
+    # folder_name = os.path.basename(folder)
+    # basedir = os.path.dirname(folder)
     rep_folder = os.path.join(folder, rep)
     if not os.path.isdir(rep_folder):
-        return
-    result = subprocess.run(["tko", "rep", "graph", "-f", rep_folder, "-W", "100", "-H", "15"], capture_output=True, text=True)
+        return ""
+    result = subprocess.run(["tko", "rep", "graph", "--mono", "-f", rep_folder, "-W", "100", "-H", "15"], capture_output=True, text=True)
     output = result.stdout
-    with open(os.path.join(basedir, folder_name + "_graph.txt"), "w", encoding="utf-8") as graph_file:
-        graph_file.write(output)
+    # with open(os.path.join(basedir, folder_name + "_graph.txt"), "w", encoding="utf-8") as graph_file:
+    #     graph_file.write(output)
+    return output
 
-def get_user_tasks(target: Target):
+def get_user_resume(target: Target):
     folder = target.folder
     rep_name = target.rep
     print(".." + folder + ".." + rep_name)
@@ -44,18 +61,56 @@ def get_user_tasks(target: Target):
     with open(os.path.join(basedir, folder_name + "_tasks.yaml"), "w", encoding="utf-8") as tasks_file:
         tasks_file.write(output)
 
-def load_folders_from_jsons(jsons: list[str]) -> list[Target]:
-    folders: list[Target] = []
-    for file in jsons:
-        with open(file, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            class_folders: str = data["folder"]
-            rep: str = data["rep"]
-            if "folder" in data:
-                student_list: list[str] = os.listdir(class_folders)
-                student_target = [Target(os.path.join(class_folders, student), rep) for student in student_list]
-                folders.extend(student_target)
-    return folders
+def load_folders_from_jsons(file: str) -> list[Target]:
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        task: JsonTask = JsonTask()
+        task.load_from_json(data)
+        task.path = file
+        class_folders: str = task.folder
+        rep: str = task.rep
+        student_list: list[str] = os.listdir(class_folders)
+        student_target = [Target(os.path.join(class_folders, student), rep) for student in student_list]
+        return student_target
+        
+ 
+def process_task_jsons(args: argparse.Namespace):
+    jsons: list[str] = args.jsons
+    
+    for json_file in jsons:
+        repo_list: list[Target] = load_folders_from_jsons(json_file)
+
+        if args.graph:
+            output_file = os.path.splitext(json_file)[0] + "_graph.txt"
+            with open(output_file, "w", encoding="utf-8") as graph_file:
+                for repo in repo_list:
+                    graph_file.write("-" * 120 + "\n")
+                    graph_file.write(repo.folder + "\n")
+                    graph = get_user_graph(repo)
+                    graph_file.write(graph + "\n")
+
+
+        if args.info:
+            for repo in repo_list:
+                get_user_resume(repo)
+
+def process_task_folders(args: argparse.Namespace):
+    repo_list: list[Target] = []
+    for folder in args.folders:
+        if not os.path.isdir(folder):
+            print(f"Folder {folder} does not exist.")
+            continue
+        repo_list.append(Target(folder, args.rep))
+
+    repo_list = sorted(repo_list, key=lambda x: x.folder)
+    if args.graph:
+        for repo in repo_list:
+            get_user_graph(repo)
+
+    if args.info:
+        for repo in repo_list:
+            get_user_resume(repo)
+
 
 def main():
     # Caminho do arquivo CSV
@@ -77,26 +132,14 @@ def main():
         exit(1)
         return
     
-    targets: list[Target] = []
     if args.jsons:
-        targets.extend(load_folders_from_jsons(args.jsons))
+        process_task_jsons(args)
 
     if args.folders:
-        for folder in args.folders:
-            if not os.path.isdir(folder):
-                print(f"Folder {folder} does not exist.")
-                continue
-            targets.append(Target(folder, args.rep))
+        process_task_folders(args)
 
-    targets = sorted(targets, key=lambda x: x.folder)
 
-    if args.graph:
-        for target in targets:
-            get_user_graph(target)
 
-    if args.info:
-        for target in targets:
-            get_user_tasks(target)
 
 if __name__ == "__main__":
     main()
